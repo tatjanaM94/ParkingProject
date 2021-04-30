8ddfc45b3021c315ae6b9814d5ce59eb820cd6de
Tatjana <tance_m@hotmail.com>
2021-04-30 21:48:42 +0200

    injection

---
+++ ParkingProject.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ParkingProject.Application.Interfaces;
+using ParkingProject.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingProject.Infrastructure.IoC
+{
+    public static class DependencyContainer
+    {
+        public static void AddIoCService(this IServiceCollection services)
+        {
+            // application
+            services.AddScoped<IGarageService, GarageService>();
+            
+            //  domain.interfaces-> infrastructure.data
+
+        }
+    }
+}
---
+++ ParkingProject.MVC/Controllers/GaragesController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ParkingProject.Application.Interfaces;
+using ParkingProject.MVC.Models.Garages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingProject.MVC.Controllers
+{
+    public class GaragesController : Controller
+    {
+        private readonly IGarageService _garageService;
+        private readonly IMapper _mapper;
+
+        public GaragesController(IGarageService garageService, IMapper mapper)
+        {
+            _garageService = garageService;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            var allGarages = _garageService.GetGarages();
+            return View(_mapper.Map<List<GarageViewModel>>(allGarages));
+
+        }
+    }
+}
