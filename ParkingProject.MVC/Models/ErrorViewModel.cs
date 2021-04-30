2400955329646a3b5124d873e31c84ac7e742f8d
Tatjana <tance_m@hotmail.com>
2021-04-30 21:04:57 +0200

    repository

---
+++ ParkingProject.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ParkingProject.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
