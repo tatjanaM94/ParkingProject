247c0fc4c5fe303a67d39b48b0943d0e6bf8bdbd
Tatjana <tance_m@hotmail.com>
2021-04-30 21:52:40 +0200

    repository.data

---
+++ ParkingProject.Infrastucture.Data/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingProject.Domain.Interfaces.Base;
+using ParkingProject.Infrastucture.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParkingProject.Infrastucture.Data.Repositories.Base
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        
+        private readonly LibraryDbContext _dbContext;
+        public BaseRepository(LibraryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public T Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            return entity;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public IReadOnlyList<T> GetAll()
+        {
+            return _dbContext.Set<T>().ToList();
+        }
+
+        public T GetById(Guid id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
